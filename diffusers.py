fa957d4e2d63b332b8d8d89ef85b72f0b5846fa8
Weiwei Wang <xihanshuowww@163.com>
2025-03-14 09:15:51 -0500

    Update diffusers.py

--- diffusers.py
+++ diffusers.py
@@ -1,5 +1,5 @@
 # !pip install transformers diffusers
-%matplotlib inline
+
 from matplotlib import pylot as plt
 from diffusers import StableDiffusionPipeline
 
@@ -8,9 +8,14 @@ model_id = ""
 # Load the pipeline
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
+# 
+num_inference_steps = 50
+guidance_scale = 7.5  
+
+
 # run pipeline in inference (sample random noise and denoise)
 prompt = "A painting of a squirrel eating a burger"
-images = pipe([prompt], num_inference_steps=50, eta=0.3, guidance_scale=6) ["sample"]
+images = pipe([prompt], num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, eta=0.3) ["sample"]
 
 
 plt.imshow(images[0])
